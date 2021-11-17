7a0c879ecb71ff73b1a2d85405b13784242ee6d3
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-17 14:58:12 -0600

    removed the next.config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  reactStrictMode: true,
-  env: {
-    ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
-    SECRET_KEY: process.env.SECRET_KEY,
-    REGION: process.env.REGION,
-    TABLE_NAME: process.env.TABLE_NAME
-  }
+  reactStrictMode: true
 }
