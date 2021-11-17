1f31340afd66ef00ec7e94d54fd07f92ca087d65
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-17 08:20:05 -0600

    prettier and deleted some early next files

--- pages/api/index.js
+++ pages/api/index.js
@@ -3,14 +3,22 @@ import fs from 'fs'
 import path from 'path'
 
 export default function handler(req, res) {
+  // process GET request
+  if (req.method === 'GET') {
+    res.status(200).json(list)
+  }
+
+  // process PUT request
   if (req.method === 'PUT') {
-    // process PUT request
+    // for this project, body is only one entry at a time
+    // to update multiple entries, PUT must be called multiple times
     const body = req.body
-    let objIndex = list.findIndex(obj => obj.id === body.id)
+    let objIndex = list.findIndex((obj) => obj.id === body.id)
     list[objIndex] = body
+
     const dir = path.resolve('./data')
-    console.log('dir', dir)
     fs.writeFileSync(dir + '/list.json', JSON.stringify(list, null, 2))
+
     res.status(201).json(list)
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,17 @@ import styles from '../styles/Home.module.css'
 import list from '../data/list.json'
 
 export default function Home() {
-  const [people, setPeople] = useState(list.map(name => name))
+  const [people, setPeople] = useState(list.map((name) => name))
   const selectRef = React.createRef()
   const [chosen, setChosen] = useState()
 
   function forceFourth(remaining, id) {
     // check if one couple has had no choices yet
-    remaining = remaining.filter(p => p.id !== id)
+    remaining = remaining.filter((p) => p.id !== id)
     let coupleHash = {}
     for (let i = 0; i < remaining.length; i++) {
       if (remaining[i].partnerId in coupleHash) {
-        return [ list[remaining[i].id], list[remaining[i].partnerId] ]
+        return [list[remaining[i].id], list[remaining[i].partnerId]]
       }
       coupleHash[remaining[i].partnerId] = remaining[i].partnerid
     }
@@ -28,16 +28,19 @@ export default function Home() {
     // if this sixth person is the last of the first 3 couples, and the last couple that hasn't chosen yet
     //    still has an available chosen, the sixth person must pick the available one from that couple
     // check if two of the entries are from the same couple
-    let remainingToPick = list.filter(p => !p.hasChosen)
+    let remainingToPick = list.filter((p) => !p.hasChosen)
     // if 2 of these ^^ have the same coupleId and one has not been chosen, must return that one
     let coupleHash = {}
     let duplicate = false
     for (let i = 0; i < remainingToPick.length; i++) {
-      if (remainingToPick[i].coupleId in coupleHash) { 
+      if (remainingToPick[i].coupleId in coupleHash) {
         let person1 = remainingToPick[i]
         let person2 = remainingToPick[coupleHash[remainingToPick[i].coupleId]]
-        if (!person1.hasBeenChosen && person1.id !== id) { return [person1] }
-        else if (!person2.hasBeenChosen && person2.id !== id) { return [person2] }
+        if (!person1.hasBeenChosen && person1.id !== id) {
+          return [person1]
+        } else if (!person2.hasBeenChosen && person2.id !== id) {
+          return [person2]
+        }
       }
       coupleHash[remainingToPick[i].coupleId] = i
     }
@@ -47,23 +50,31 @@ export default function Home() {
   function handleClick() {
     // find current id for who is picking and the remaining people that have not been chosen
     let id = selectRef.current.selectedIndex
-    let remaining = people.filter(p => !p.hasBeenChosen)
-    
+    let remaining = people.filter((p) => !p.hasBeenChosen)
+    if (list[id].hasChosen) {
+      setChosen('YOU HAVE ALREADY PICKED')
+      return
+    }
+
     let allowableList
     // edge case for 4th pick
     if (remaining.length === 5) {
       allowableList = forceFourth(remaining, id)
-    } 
+    }
     // edge case for 6th pick
-    else if (remaining.length === 3) { 
+    else if (remaining.length === 3) {
       allowableList = forceSixth(remaining, id)
-    } 
+    }
     // every other option
     else {
-      allowableList = people.filter(p => {
-      return people[id].coupleId !== p.coupleId && !p.hasBeenChosen
-        && people[people[id].partnerId].choseeCoupleId !== p.coupleId
-    })}
+      allowableList = people.filter((p) => {
+        return (
+          people[id].coupleId !== p.coupleId &&
+          !p.hasBeenChosen &&
+          people[people[id].partnerId].choseeCoupleId !== p.coupleId
+        )
+      })
+    }
 
     // chooses a "random" person from those that are left
     let chosenOne
@@ -74,7 +85,7 @@ export default function Home() {
       chosenOne = allowableList[random]
     }
     setChosen(chosenOne.santa)
-    
+
     // put request parameter update for the person that just picked
     let chooserParams = people[id]
     chooserParams.hasChosen = true
@@ -88,17 +99,17 @@ export default function Home() {
     postData(`/api`, chosenParams)
   }
 
-  async function postData(url = '', data={}) {
+  async function postData(url = '', data = {}) {
     const response = await fetch(url, {
       method: 'PUT',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data)
+      body: JSON.stringify(data),
     })
     return response.json()
   }
-  
+
   return (
     <div className={styles.container}>
       <Head>
@@ -106,9 +117,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Tanner 2021 Secret Santa Picker!
-        </h1>
+        <h1 className={styles.title}>Tanner 2021 Secret Santa Picker!</h1>
 
         <p className={styles.description}>
           {`Guaranteed to not let you pick your own partner's name!`}
@@ -116,15 +125,23 @@ export default function Home() {
 
         <div className={styles.grid}>
           <div className={styles.row}>
-            <label className={styles.description} htmlFor="list">Your name: </label>
+            <label className={styles.description} htmlFor="list">
+              Your name:{' '}
+            </label>
             <select className={styles.list} name="list" ref={selectRef}>
-              {people.map(p => (
-                <option className={styles.option} key={p.santa} value={p.santa}>{p.santa}</option>
+              {people.map((p) => (
+                <option className={styles.option} key={p.santa} value={p.santa}>
+                  {p.santa}
+                </option>
               ))}
             </select>
           </div>
-          <button className={styles.button} onClick={handleClick}>PICK MY PERSON</button>
-          <h2 className={styles.description}>You will be Santa for: {chosen}</h2>
+          <button className={styles.button} onClick={handleClick}>
+            PICK MY PERSON
+          </button>
+          <h2 className={styles.description}>
+            You will be Santa for: {chosen}
+          </h2>
         </div>
       </main>
     </div>
