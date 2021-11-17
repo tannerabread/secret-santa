0b6c80ae3a0eeee32bb73406961409ced2d15428
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-17 16:53:54 -0600

    thank you stackoverflow for making me look for a typo

--- db.js
+++ db.js
@@ -3,8 +3,8 @@ import AWS from 'aws-sdk'
 // Update AWS config
 AWS.config.update({
   // AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY are reserved variable names on Vercel
-  accesKeyId: process.env.DB_ACCESS_KEY_ID,
-  secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
+  accesKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_KEY,
   region: process.env.REGION,
 })
 
