56f89847ae63d9ce74632670d8fd007b8d9c60c2
Bannon Tanner <bannon.n.tanner@gmail.com>
2021-11-19 09:18:53 -0600

    tried adding env back to next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  env: {
+    DYNAMODB_ACCESS_KEY: process.env.DYNAMODB_ACCESS_KEY,
+    DYNAMODB_SECRET_ACCESS_KEY: process.env.DYNAMODB_SECRET_ACCESS_KEY,
+    DYNAMODB_REGION: process.env.DYNAMODB_REGION,
+    DYNAMODB_TABLE_NAME: process.env.DYNAMODB_TABLE_NAME
+  }
 }
